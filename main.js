b61fe5bc486253f1be638abbc7efb94dcf2ef69d
gabrielcastellani <gabrielcastellanioliveira@gmail.com>
2022-02-19 09:08:21 -0300

    Alterando serviço mqtt

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
-// const express = require('express')
-// const cors = require('cors');
-// const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const mqtt = require("mqtt");
-let value = "";
+let thermometerValue = "";
 
-// var allowlist = ['http://localhost:5500']
-// var corsOptionsDelegate = function (req, callback) {
-//   var corsOptions;
-//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false } // disable CORS for this request
-//   }
-//   callback(null, corsOptions) // callback expects two parameters: error and options
-// }
+var allowlist = ['http://localhost:5500']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }
+    } else {
+        corsOptions = { origin: false }
+    }
 
-// app.get('/', cors(corsOptionsDelegate), function (req, res) {
-//     res.send({
-//         value: value
-//     })
-//   })
-  
-//   app.listen(3000)
+    callback(null, corsOptions);
+}
 
-const client = mqtt.connect("mqtt://au1.cloud.thethings.network:1883", { 
+const client = mqtt.connect("mqtt://au1.cloud.thethings.network:1883", {
     clientId: "mqttClient01",
     username: "lmic@ttn",
     password: "NNSXS.4U2IUVA7CRNUPYY6UXC7WB7GIKKSJLXUOREY6OI.GR5N3GEPVTUAHYEI3BCCAMU7HQCTT4HGUU7QC7FYVHM7YVKD4YVA",
 });
 
-client.on("message", function(topic, message) {
+client.on("message", function (topic, message) {
     const object = JSON.parse(message);
-    console.log(message);
-    if(object.end_device_ids.join_eui == "0000000000000005") {
+
+    if (object.end_device_ids.join_eui == "0000000000000000") {
         const buffer = new Buffer.from(object.uplink_message.frm_payload, "base64");
-        value = buffer.toString();
-        console.log(value)
+        thermometerValue = buffer.toString();
     }
 });
 
-client.on("connect", function() {
+client.on("connect", function () {
     console.log("Connected");
     client.subscribe(["v3/lmic@ttn/devices/eui-88571dfffeee8c47/up", "v3/lmic@ttn/devices/#"], () => {
-      console.log("Subscribe to topic");  
+        console.log("Subscribe to topic");
     })
 });
 
-client.on("error", function(error) {
+client.on("error", function (error) {
     console.log("Can't connect " + error);
     process.exit(1);
 });
+
+app.get('/', cors(corsOptionsDelegate), function (req, res) {
+    res.send({
+        thermometerValue: thermometerValue
+    })
+})
+
+app.listen(3000)
